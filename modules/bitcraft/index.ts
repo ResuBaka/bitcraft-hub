16fd2d6082c32ef9ece70124f3fda2bb821d3203
ResuBaka <resubaka@gmail.com>
2024-04-20 14:05:43 +0200

    feat: file base routing in bitcraft module

--- modules/bitcraft/index.ts
+++ modules/bitcraft/index.ts
@@ -1,6 +1,6 @@
 // `nuxt/kit` is a helper subpath import you can use when defining local modules
 // that means you do not need to add `@nuxt/kit` to your project's dependencies
-import {createResolver, defineNuxtModule, addServerHandler, addServerImportsDir} from 'nuxt/kit'
+import {createResolver, defineNuxtModule, addServerScanDir} from 'nuxt/kit'
 
 export default defineNuxtModule({
     meta: {
@@ -9,6 +9,6 @@ export default defineNuxtModule({
     setup () {
         // Add an API route
         const resolver = createResolver(import.meta.url)
-        addServerImportsDir(resolver.resolve('./runtime/server'))
+        addServerScanDir(resolver.resolve('./runtime/server'), { prepend: true })
     }
 })
