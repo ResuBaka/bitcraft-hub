7b85785b7fb26015248687110d29ad9c39508bd1
ResuBaka <resubaka@gmail.com>
2024-04-20 14:03:49 +0200

    feat: return json and handle basic error

--- modules/bitcraft/index.ts
+++ modules/bitcraft/index.ts
@@ -1,18 +1,14 @@
 // `nuxt/kit` is a helper subpath import you can use when defining local modules
 // that means you do not need to add `@nuxt/kit` to your project's dependencies
-import { createResolver, defineNuxtModule, addServerHandler } from 'nuxt/kit'
+import {createResolver, defineNuxtModule, addServerHandler, addServerImportsDir} from 'nuxt/kit'
 
 export default defineNuxtModule({
     meta: {
         name: 'hello'
     },
     setup () {
-        const { resolve } = createResolver(import.meta.url)
-
         // Add an API route
-        addServerHandler({
-            route: '/api/hello',
-            handler: resolve('./runtime/api-route')
-        })
+        const resolver = createResolver(import.meta.url)
+        addServerImportsDir(resolver.resolve('./runtime/server'))
     }
 })
--- modules/bitcraft/runtime/SQLRequest.ts
+++ modules/bitcraft/runtime/SQLRequest.ts
@@ -1,9 +1,15 @@
-export default async function SQLRequest(body) {
-    await fetch("https://alpha-playtest-1.spacetimedb.com/database/sql/bitcraft-alpha", {
+export default async function SQLRequest<T>(body: string): Promise<T> {
+    const response =  await fetch("https://alpha-playtest-1.spacetimedb.com/database/sql/bitcraft-alpha", {
         method:"POST",
         headers: {
             "Authorization": "Basic dG9rZW46ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKRlV6STFOaUo5LmV5Sm9aWGhmYVdSbGJuUnBkSGtpT2lJeFpXUXlZelJsWVRsbVlUVmtaVFZqTURKaVltVTBNMkV6TldFd05XSTVabVZsTlRVek9ESmhPR0l5WldZd04yRTVaVEk0TnprMU1qUXlPR1ZqTVdFNUlpd2lhV0YwSWpveE56RXpOVFkwTkRZekxDSmxlSEFpT201MWJHeDkua2cyUHBfQ0t5OE1hcTJBT0xDeW0tckRneENkaS01MUZZV05JZ0VhQjJhMnB0YVNTRk11cGdUOXFOVWp3NVlfYkxHOERGcV8yRkxTLWhBRmVmbEU2SFE="
         },
         body: body
     })
+
+    if (!response.ok) {
+        throw new Error(await response.text());
+    }
+
+    return await response.json();
 }
