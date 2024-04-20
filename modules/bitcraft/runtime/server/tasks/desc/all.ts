c7bb50c86b22c161d4e784b1b051da542b0efdd4
ResuBaka <resubaka@gmail.com>
2024-04-20 14:38:24 +0200

    refactor: move fetch of all decs to nitro task

--- modules/bitcraft/runtime/server/tasks/desc/all.ts
+++ modules/bitcraft/runtime/server/tasks/desc/all.ts
@@ -1,5 +1,5 @@
 import { readFile, writeFile } from "node:fs/promises";
-import SQLRequest from "./SQLRequest";
+import SQLRequest from "./../../../SQLRequest";
 let rootFolder = `${process.cwd()}/storage/Desc`;
 let allDescTables = [
     "AchievementDesc",
@@ -62,11 +62,19 @@ let allDescTables = [
     "VehicleDesc",
     "WeaponDesc",
     "WeaponTypeDesc"
-  ]  
-export default async function updateAllDesc() {
-    for (var descTable of allDescTables) {
-        const sql = `SELECT * FROM ${descTable}`
-        const result = await SQLRequest<any>(sql)
-        await writeFile(`${rootFolder}/${descTable}.json`, JSON.stringify(result))
-   }
-}
+]
+export default defineTask({
+    meta: {
+        name: "fetch:all:desc",
+        description: "Run database migrations",
+    },
+    async run({ payload, context }) {
+        for (var descTable of allDescTables) {
+            const sql = `SELECT * FROM ${descTable}`
+            const result = await SQLRequest<any>(sql)
+            await writeFile(`${rootFolder}/${descTable}.json`, JSON.stringify(result))
+        }
+
+        return { result: "Success" };
+    },
+});
