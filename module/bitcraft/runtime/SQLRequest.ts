ea9f0f46778623d136d0263cc3570808488285d2
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2024-04-20 13:45:04 +0200

    Add SQLRequest

---
+++ module/bitcraft/runtime/SQLRequest.ts
@@ -0,0 +1,9 @@
+export default async function SQLRequest(body) {
+    await fetch("https://alpha-playtest-1.spacetimedb.com/database/sql/bitcraft-alpha", {
+        method:"POST",
+        headers: {
+            "Authorization": "Basic dG9rZW46ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKRlV6STFOaUo5LmV5Sm9aWGhmYVdSbGJuUnBkSGtpT2lJeFpXUXlZelJsWVRsbVlUVmtaVFZqTURKaVltVTBNMkV6TldFd05XSTVabVZsTlRVek9ESmhPR0l5WldZd04yRTVaVEk0TnprMU1qUXlPR1ZqTVdFNUlpd2lhV0YwSWpveE56RXpOVFkwTkRZekxDSmxlSEFpT201MWJHeDkua2cyUHBfQ0t5OE1hcTJBT0xDeW0tckRneENkaS01MUZZV05JZ0VhQjJhMnB0YVNTRk11cGdUOXFOVWp3NVlfYkxHOERGcV8yRkxTLWhBRmVmbEU2SFE="
+        },
+        body: body
+    })
+}
