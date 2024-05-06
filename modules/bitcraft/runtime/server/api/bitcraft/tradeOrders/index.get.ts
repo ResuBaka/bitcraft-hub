7c319a2c34a47c73fc97757214f2864a37a46f4d
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2024-05-06 14:42:34 +0200

    formated code

--- modules/bitcraft/runtime/server/api/bitcraft/tradeOrders/index.get.ts
+++ modules/bitcraft/runtime/server/api/bitcraft/tradeOrders/index.get.ts
@@ -1,5 +1,11 @@
-import { getCargoDescRowsFromRows, readCargoDescRows } from "~/modules/bitcraft/gamestate/cargoDesc";
-import { getItemRowsFromRows, readItemRows } from "~/modules/bitcraft/gamestate/item";
+import {
+  getCargoDescRowsFromRows,
+  readCargoDescRows,
+} from "~/modules/bitcraft/gamestate/cargoDesc";
+import {
+  getItemRowsFromRows,
+  readItemRows,
+} from "~/modules/bitcraft/gamestate/item";
 import {
   getTradingOrderStateRowsFromRows,
   readTradeOrderStateRows,
@@ -10,7 +16,12 @@ import {
 const items = getItemRowsFromRows(readItemRows());
 const cargo_rows = getCargoDescRowsFromRows(readCargoDescRows());
 const rows = replaceTradeOrdersItemIdWithItem(
-  replaceTradeOrdersCargoIdWithCargo(getTradingOrderStateRowsFromRows(readTradeOrderStateRows()),cargo_rows), items);
+  replaceTradeOrdersCargoIdWithCargo(
+    getTradingOrderStateRowsFromRows(readTradeOrderStateRows()),
+    cargo_rows,
+  ),
+  items,
+);
 
 export default defineEventHandler((event) => {
   let { search, page, perPage } = getQuery(event);
@@ -26,15 +37,20 @@ export default defineEventHandler((event) => {
     perPage = 16;
   }
   const rowsFilterted = rows.filter(
-    (trade_order) => !search || trade_order.offer_items.filter((item) => {
+    (trade_order) =>
+      !search ||
+      trade_order.offer_items.filter((item) => {
         return item.item.name.toLowerCase().includes(search.toLowerCase());
-      }).length > 0 || trade_order.required_items.filter((item) => {
+      }).length > 0 ||
+      trade_order.required_items.filter((item) => {
         return item.item.name.toLowerCase().includes(search.toLowerCase());
-      }).length > 0  || trade_order.offer_cargo.filter((cargo) => {
+      }).length > 0 ||
+      trade_order.offer_cargo.filter((cargo) => {
         return cargo.name.toLowerCase().includes(search.toLowerCase());
-      }).length > 0 || trade_order.required_cargo.filter((cargo) => {
+      }).length > 0 ||
+      trade_order.required_cargo.filter((cargo) => {
         return cargo.name.toLowerCase().includes(search.toLowerCase());
-      }).length > 0 
+      }).length > 0,
   );
   return {
     trade_orders: rowsFilterted.slice((page - 1) * perPage, page * perPage),
