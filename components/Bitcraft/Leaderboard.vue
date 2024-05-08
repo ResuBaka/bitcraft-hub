daf39ca3a36b780dd7a13681913b62135ff74e14
Gix <gix.neidhaart@gmail.com>
2024-05-08 20:22:47 +0200

    Update template from suggestions

--- components/Bitcraft/Leaderboard.vue
+++ components/Bitcraft/Leaderboard.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+const numberFormat = new Intl.NumberFormat(undefined);
+
 function entityIdToName(entityId: number): string {
   if (!leaderboard.value?.players) {
     return entityId.toString();
@@ -33,9 +35,6 @@ const topPlayersByLvl = computed(() => {
 });
 
 let selectedSkills = ref("Fishing");
-const selectedCategory = computed(() => {
-  return selectedSkills.value;
-});
 </script>
 
 <style scoped lang="scss">
@@ -71,103 +70,76 @@ table {
               </v-sheet>
             </div>
             <div class="d-flex justify-space-between skill-buttons mb-3">
-              <v-btn :variant="'flat'" @click="selectedSkills = 'by_exp'" :active="selectedSkills === 'by_exp'" >Total experience</v-btn>
-              <v-btn :variant="'flat'" @click="selectedSkills = 'by_level'" :active="selectedSkills === 'by_level'" >Total level</v-btn>
-              <v-btn v-for="i in skills.slice(0, 3)" :variant="'flat'" @click="selectedSkills = i.name" :active="selectedSkills === i.name">{{ i.name }}</v-btn>
+              <v-btn variant="flat" @click="selectedSkills = 'by_exp'" :active="selectedSkills === 'by_exp'" >Total experience</v-btn>
+              <v-btn variant="flat" @click="selectedSkills = 'by_level'" :active="selectedSkills === 'by_level'" >Total level</v-btn>
+              <v-btn v-for="i in skills.slice(0, 3)" variant="flat" @click="selectedSkills = i.name" :active="selectedSkills === i.name">{{ i.name }}</v-btn>
             </div>
             <div class="d-flex justify-space-between skill-buttons mb-3">
-              <v-btn v-for="i in skills.slice(3, 8)" :variant="'flat'" @click="selectedSkills = i.name" :active="selectedSkills === i.name">{{ i.name }}</v-btn>
+              <v-btn v-for="i in skills.slice(3, 8)" variant="flat" @click="selectedSkills = i.name" :active="selectedSkills === i.name">{{ i.name }}</v-btn>
             </div>
             <div class="d-flex justify-space-between skill-buttons mb-3">
-              <v-btn v-for="i in skills.slice(8, 14)" :variant="'flat'" @click="selectedSkills = i.name" :active="selectedSkills === i.name">{{ i.name }}</v-btn>
+              <v-btn v-for="i in skills.slice(8, 14)" variant="flat" @click="selectedSkills = i.name" :active="selectedSkills === i.name">{{ i.name }}</v-btn>
             </div>
           </v-col>
         </v-row>
         <v-row v-if="selectedSkills !== 'by_exp' && selectedSkills !== 'by_level'">
           <v-col lass="v-col-12 pa-0">
-            <v-data-table density="compact" 
-            :items="topPlayers[selectedCategory]"
-              :headers="[
-                { title: 'Rank', value: 'rank' },
-                { title: 'Player', value: 'player' },
-                { title: 'Level', value: 'level' },
-                { title: 'Experience', value: 'exp' },
-              ]"
-              items-per-page="100"
-            >
-              <template v-slot:headers="{ columns }">
-                <tr>
-                  <template v-for="column in columns" :key="column.key">
-                    <th
-                      :class="{
-                        'text-left': column.key === 'rank',
-                        'text-center':
-                          column.key === 'player' || column.key === 'level',
-                        'text-right': column.key === 'exp',
-                      }"
-                    >
-                      <span>{{ column.title }}</span>
-                    </th>
-                  </template>
-                </tr>
-              </template>
-              <template v-slot:item="{ item, index }">
-                <tr>
-                  <td># {{ index + 1 }}</td>
-                  <td class="text-center">
-                    <NuxtLink :to="{ path: 'players/' + item.entity_id }">
-                      {{ entityIdToName(item.entity_id) }}
-                    </NuxtLink>
-                  </td>
-                  <td class="text-center">
-                    {{ item.experience_stacks[selectedSkills].level }}
-                  </td>
-                  <td class="text-right">
-                    {{ item.experience_stacks[selectedSkills].experience }}
-                  </td>
-                </tr>
-              </template>
-              <template #bottom></template>
-            </v-data-table>
+            <v-data-table density="compact"
+                                    :items="topPlayers[selectedSkills]"
+                                    :headers="[
+                                    { title: 'Rank', value: 'rank', align: 'start' },
+                                    { title: 'Player', value: 'player', align: 'center' },
+                                    { title: 'Level', value: 'level', align: 'center' },
+                                    { title: 'Experience', value: 'exp', align: 'end'}
+                                ]"
+                                    hover
+                                    items-per-page="100"
+                        >
+                        <template v-slot:item.rank="{ index }">
+                          # {{ index + 1 }}
+                        </template>
+                        <template v-slot:item.level="{ item }">
+                          {{ item.experience_stacks[selectedSkills].level }}
+                        </template>
+                        <template v-slot:item.player="{ item }">
+                          <NuxtLink :to="{ path: 'players/' + item.entity_id }">
+                            {{ entityIdToName(item.entity_id) }}
+                          </NuxtLink>
+                        </template>
+                        <template v-slot:item.exp="{ item }">
+                          {{ numberFormat.format(item.experience_stacks[selectedSkills].experience) }}
+                        </template>
+                        <template #bottom></template>
+                      </v-data-table>
           </v-col>
         </v-row>
         <v-row v-if="selectedSkills === 'by_exp'">
           <v-col lass="v-col-12 pa-0">
             <v-data-table
               density="compact"
-              :items="topPlayersByExp"
+              :items="topPlayers[selectedSkills]"
               :headers="[
-                { title: 'Rank', value: 'rank' },
-                { title: 'Player', value: 'player' },
-                { title: 'Total Experience', value: 'exp' },
+                { title: 'Rank', value: 'rank', align: 'start' },
+                { title: 'Player', value: 'player', align: 'center' },
+                { title: 'Level', value: 'level', align: 'center' },
+                { title: 'Experience', value: 'exp', align: 'end' },
               ]"
+              hover
               items-per-page="100"
             >
-              <template v-slot:headers="{ columns }">
-                <tr>
-                  <template v-for="column in columns" :key="column.key">
-                    <th
-                      :class="{
-                        'text-left': column.key === 'rank',
-                        'text-center': column.key === 'player',
-                        'text-right': column.key === 'exp',
-                      }"
-                    >
-                      <span>{{ column.title }}</span>
-                    </th>
-                  </template>
-                </tr>
+              <template v-slot:item.rank="{ index }">
+                # {{ index + 1 }}
               </template>
-              <template v-slot:item="{ item, index }">
-                <tr>
-                  <td># {{ index + 1 }}</td>
-                  <td class="text-center">
-                    <NuxtLink :to="{ path: 'players/' + item.entity_id }">
-                      {{ entityIdToName(item.entity_id) }}
-                    </NuxtLink>
-                  </td>
-                  <td class="text-right">{{ item.exp }}</td>
-                </tr>
+              <template v-slot:item.level="{ item }">
+                {{ item.experience_stacks[selectedSkills].level }}
+              </template>
+              <template v-slot:item.player="{ item }">
+                <NuxtLink :to="{ path: 'players/' + item.entity_id }">
+                  {{ entityIdToName(item.entity_id) }}
+                </NuxtLink>
+              </template>
+              <template v-slot:item.exp="{ item }">
+                {{ item.experience_stacks[selectedSkills].experience }}
               </template>
               <template #bottom></template>
             </v-data-table>
