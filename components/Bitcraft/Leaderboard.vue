7f4a33e47cbf6d28d70fb5fc014e0b25efb58b96
ResuBaka <resubaka@gmail.com>
2024-05-16 19:33:59 +0200

    ui: improve leaderboard spacing

--- components/Bitcraft/Leaderboard.vue
+++ components/Bitcraft/Leaderboard.vue
@@ -42,100 +42,96 @@ let skillMenu = computed(() => {
         <v-progress-circular indeterminate> </v-progress-circular>
       </v-row>
   </v-container>
-  <template v-else-if="!pending">
-    <div>
-      <v-container class="mb-6 pa-0">
-        <v-row align="start">
-          <v-col class="v-col-12 pa-0">
-            <div class="mb-2">
-              <v-sheet class="pa-2 ma-0 text-center text-md-left text-xl-center">
-                <h1>Leaderboards</h1>
-              </v-sheet>
-            </div>
-          </v-col>
-          <v-col v-for="skill in skillMenu" :key="skill.key"
-             :style="$vuetify.display.lgAndUp ? ' flex: 1 0 18%;' : ''"
-             cols="12"
-             sm="4"
-          >
-            <v-btn variant="flat" block @click="selectedSkills = skill.key" :active="selectedSkills === skill.key" >{{ skill.text }}</v-btn>
-          </v-col>
-        </v-row>
-        <v-row v-if="selectedSkills !== 'Experience' && selectedSkills !== 'Level'">
-          <v-col lass="v-col-12 pa-0">
-            <v-table hover>
-              <thead>
-              <tr>
-                <th>Rank</th>
-                <th class="text-center">Player</th>
-                <th class="text-center">level</th>
-                <th class="text-end">Experience</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="(item, index) in leaderboard[selectedSkills]" :key="item.player_id">
-                <td>{{ index + 1 }}</td>
-                <td class="text-center">
-                  <NuxtLink class="text-decoration-none text-high-emphasis font-weight-black" :to="{ path: 'players/' + item.player_id }">
-                    {{ item.player_name }}
-                  </NuxtLink>
-                </td>
-                <td class="text-center" >{{ item.level }}</td>
-                <td class="text-end" >{{ numberFormat.format(item.experience) }}</td>
-              </tr>
-              </tbody>
-            </v-table>
-          </v-col>
-        </v-row>
-        <v-row v-if="selectedSkills === 'Experience'">
-          <v-col lass="v-col-12 pa-0">
-            <v-table hover>
-              <thead>
-              <tr>
-                <th>Rank</th>
-                <th class="text-center">Player</th>
-                <th class="text-end">Experience</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="(item, index) in leaderboard[selectedSkills]" :key="item.player_id">
-                <td>{{ index + 1 }}</td>
-                <td class="text-center">
-                  <NuxtLink class="text-decoration-none text-high-emphasis font-weight-black" :to="{ path: 'players/' + item.player_id }">
-                    {{ item.player_name }}
-                  </NuxtLink>
-                </td>
-                <td class="text-end" >{{ numberFormat.format(item.experience) }}</td>
-              </tr>
-              </tbody>
-            </v-table>
-          </v-col>
-        </v-row>
-        <v-row v-if="selectedSkills === 'Level'">
-          <v-col lass="v-col-12 pa-0">
-            <v-table hover>
-              <thead>
-                <tr>
-                  <th>Rank</th>
-                  <th class="text-center">Player</th>
-                  <th class="text-end">Level</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(item, index) in leaderboard[selectedSkills]" :key="item.player_id">
-                  <td>{{ index + 1 }}</td>
-                  <td class="text-center">
-                    <NuxtLink class="text-decoration-none text-high-emphasis font-weight-black" :to="{ path: 'players/' + item.player_id }">
-                      {{ item.player_name }}
-                    </NuxtLink>
-                  </td>
-                  <td class="text-end" >{{ numberFormat.format(item.level) }}</td>
-                </tr>
-              </tbody>
-            </v-table>
-          </v-col>
-        </v-row>
-      </v-container>
-    </div>
-  </template>
+  <v-container fluid v-else-if="!pending">
+    <v-row align="start">
+      <v-col class="v-col-12">
+        <div class="mb-2">
+          <v-sheet class="text-center text-md-left text-xl-center">
+            <h1 class="pl-md-3 pl-xl-0">Leaderboards</h1>
+          </v-sheet>
+        </div>
+      </v-col>
+      <v-col v-for="skill in skillMenu" :key="skill.key"
+         :style="$vuetify.display.lgAndUp ? ' flex: 1 0 18%;' : ''"
+         cols="12"
+         sm="4"
+      >
+        <v-btn variant="flat" block @click="selectedSkills = skill.key" :active="selectedSkills === skill.key" >{{ skill.text }}</v-btn>
+      </v-col>
+    </v-row>
+    <v-row v-if="selectedSkills !== 'Experience' && selectedSkills !== 'Level'">
+      <v-col lass="v-col-12 pa-0">
+        <v-table hover>
+          <thead>
+          <tr>
+            <th>Rank</th>
+            <th class="text-center">Player</th>
+            <th class="text-center">level</th>
+            <th class="text-end">Experience</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item, index) in leaderboard[selectedSkills]" :key="item.player_id">
+            <td>{{ index + 1 }}</td>
+            <td class="text-center">
+              <NuxtLink class="text-decoration-none text-high-emphasis font-weight-black" :to="{ path: 'players/' + item.player_id }">
+                {{ item.player_name }}
+              </NuxtLink>
+            </td>
+            <td class="text-center" >{{ item.level }}</td>
+            <td class="text-end" >{{ numberFormat.format(item.experience) }}</td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+    <v-row v-if="selectedSkills === 'Experience'">
+      <v-col lass="v-col-12 pa-0">
+        <v-table hover>
+          <thead>
+          <tr>
+            <th>Rank</th>
+            <th class="text-center">Player</th>
+            <th class="text-end">Experience</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(item, index) in leaderboard[selectedSkills]" :key="item.player_id">
+            <td>{{ index + 1 }}</td>
+            <td class="text-center">
+              <NuxtLink class="text-decoration-none text-high-emphasis font-weight-black" :to="{ path: 'players/' + item.player_id }">
+                {{ item.player_name }}
+              </NuxtLink>
+            </td>
+            <td class="text-end" >{{ numberFormat.format(item.experience) }}</td>
+          </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+    <v-row v-if="selectedSkills === 'Level'">
+      <v-col lass="v-col-12 pa-0">
+        <v-table hover>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th class="text-center">Player</th>
+              <th class="text-end">Level</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in leaderboard[selectedSkills]" :key="item.player_id">
+              <td>{{ index + 1 }}</td>
+              <td class="text-center">
+                <NuxtLink class="text-decoration-none text-high-emphasis font-weight-black" :to="{ path: 'players/' + item.player_id }">
+                  {{ item.player_name }}
+                </NuxtLink>
+              </td>
+              <td class="text-end" >{{ numberFormat.format(item.level) }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
