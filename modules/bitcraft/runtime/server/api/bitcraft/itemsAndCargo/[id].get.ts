e6048bbbabc53095bd36a5ba8c137b9632e08b59
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2024-07-08 16:17:58 +0200

    Fixes cargo showing up as unknown

---
+++ modules/bitcraft/runtime/server/api/bitcraft/itemsAndCargo/[id].get.ts
@@ -0,0 +1,34 @@
+import {
+  getItemRowsFromRows,
+  readItemRows,
+} from "~/modules/bitcraft/gamestate/item";
+
+import {
+  getCargoDescRowsFromRows,
+  readCargoDescRows,
+} from "~/modules/bitcraft/gamestate/cargoDesc";
+
+const rows1 = getItemRowsFromRows(readItemRows());
+const rows2 = getCargoDescRowsFromRows(readCargoDescRows());
+const rows = [...rows1,...rows2]
+export default defineEventHandler((event) => {
+  const id = getRouterParam(event, "id", { decode: true });
+
+  if (!id) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Missing ID",
+    });
+  }
+
+  const item = rows.find((item) => item.id == parseInt(id));
+
+  if (!item) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Item was not found",
+    });
+  }
+
+  return item;
+});
