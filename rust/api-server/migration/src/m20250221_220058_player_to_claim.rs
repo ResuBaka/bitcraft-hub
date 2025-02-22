15edc4ae6e83997c3f308b3318d3326d755329da
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2025-02-22 15:47:51 +0100

    feat: init player_to_claim

---
+++ rust/api-server/migration/src/m20250221_220058_player_to_claim.rs
@@ -0,0 +1,92 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(PlayerToClaim::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(PlayerToClaim::PlayerId)
+                            .integer()
+                            .not_null()
+                            .primary_key(),
+                        )
+                    .col(
+                        ColumnDef::new(PlayerToClaim::ClaimId)
+                            .integer()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(PlayerToClaim::InventoryPermission)
+                            .boolean()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(PlayerToClaim::BuildPermission)
+                            .boolean()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(PlayerToClaim::OfficerPermission)
+                            .boolean()
+                            .not_null(),
+                    )
+                    .col(
+                        ColumnDef::new(PlayerToClaim::CoOwnerPermissioN)
+                            .boolean()
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+            .expect("Creating EntityToClaim Table");
+
+            manager
+            .create_index(
+                Index::create()
+                    .name("player_to_claim_player_id")
+                    .table(PlayerToClaim::Table)
+                    .col(PlayerToClaim::PlayerId)
+                    .to_owned(),
+            )
+            .await
+            .expect("Creating player_to_claim_player_id index");
+
+        manager
+            .create_index(
+                Index::create()
+                    .name("player_to_claim_claim_id")
+                    .table(PlayerToClaim::Table)
+                    .col(PlayerToClaim::ClaimId)
+                    .to_owned(),
+            )
+            .await
+            .expect("Creating player_to_claim_claim_id index");
+
+        Ok(())
+    }
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(PlayerToClaim::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum PlayerToClaim {
+    Table,
+    Id,
+    PlayerId,
+    ClaimId,
+    UserName,
+    InventoryPermission,
+    BuildPermission,
+    OfficerPermission,
+    CoOwnerPermissioN,
+}
