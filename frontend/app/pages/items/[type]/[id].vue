a3fd345122eb077bf56e0154f0d70d658b528b4e
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2025-06-08 15:10:45 +0200

    json instead of custom component

--- frontend/app/pages/items/[type]/[id].vue
+++ frontend/app/pages/items/[type]/[id].vue
@@ -18,19 +18,16 @@ const { data: allRecipiesFetch, pending: allRecipiesPending } =
   useFetchMsPack<RecipesAllResponse>(() => {
     return `${api.base}/recipes/get_all`;
   });
-let id = computed<String | Number>(() => {
-  return route.params.id;
-});
-let type = computed<"Item" | "Cargo">(() => {
-  return route.params.type;
-});
 
 
 const recipeInfo = computed(() => {
+  let hasValues =  allRecipiesFetch.value === undefined
   let allRecipies = allRecipiesFetch.value?.recipes ?? {};
   let cargo_desc = allRecipiesFetch.value?.cargo_desc ?? {};
   let item_desc = allRecipiesFetch.value?.item_desc ?? {};
   let item_list_desc = allRecipiesFetch.value?.item_list_desc ?? {};
+  let id = route.params.id
+  let type =  route.params.type
   const consumed = {
     Item: {},
     Cargo: {},
@@ -81,8 +78,92 @@ const recipeInfo = computed(() => {
       getCraftedItemStack(item_stack, recipie)
     }
   }
+
+  if(hasValues){
+    return {
+      items: [{}],
+      consumed,
+      crafted,
+    };
+  }
+
+  let item
+  if(type == "Item"){
+      item = item_desc[id]
+  }else{
+    item = cargo_desc[id]
+  }
+
+  function getCraftedChildren() {
+
+    const children = []
+    for(const recipe of crafted[type][id]){
+      let itemChildren = []
+
+      for(const item of allRecipies[recipe.id]?.crafted_item_stacks){
+        itemChildren.push({
+          id: item.item_id,
+          type: item.item_type,
+          quantity: 1,
+          children: getConsumedChildren(item.item_id,item.item_type,1,[]),
+        })
+      }
+       children.push({
+        recipe: true,
+        children: itemChildren
+      })
+      
+    }
+    return children
+  }
+
+    function getQuantity(item_quantity: number, quantity: number, recipe_id_quantity:number) {
+      return Math.ceil((quantity * item_quantity) / recipe_id_quantity)
+    }
+
+    function getConsumedChildren(id: any, type: "Item" | "Cargo", quantity: number,recipes: number[]) {
+    const children = []
+    if(crafted[type][id] == undefined){
+      return
+    }
+    for(const recipe of crafted[type][id]){
+      let itemChildren = []
+      console.log(recipes)
+      const exists =
+        recipes.findIndex((value) => value == recipe.id) !== -1;
+      if(exists){
+        continue
+      }
+      recipes.push(recipe.id);
+      console.log(recipes)
+      for(const item of allRecipies[recipe.id].consumed_item_stacks){
+        let get_qauntity = getQuantity(item.quantity,quantity, recipe.quantity)
+        itemChildren.push({
+          id: item.item_id,
+          type: item.item_type,
+          quantity: get_qauntity,
+          children: getConsumedChildren(item.item_id,item.item_type,get_qauntity, [...recipes]),
+        })
+      }
+      children.push({
+        recipe: true,
+        children: itemChildren
+      })
+      
+    }
+    return children
+  }
+  let children = getCraftedChildren()
+  console.log(JSON.stringify(children))
+  let items = [{
+      id: item.id,
+      type: item.type ,
+      quantity: 1,
+      children: children,
+  }]
+    
   return {
-    allRecipies,
+    items,
     consumed,
     crafted,
   };
@@ -95,17 +176,13 @@ useSeoMeta({
 
 <template>
   <v-container fluid>
-    <v-card v-if="recipeInfo !== undefined">
-      <v-card-text>
-        <v-list>
-            <v-list-item>
-                <v-list-item-title>How to Craft this Item </v-list-item-title>
-                <v-list-item v-for="recipe of recipeInfo.crafted[type][id]">
-                  <recusive-crafting-recipe :quantity="1" :recipies="[]" :recipeId="recipe" :recipeInfo="recipeInfo"></recusive-crafting-recipe>
-                </v-list-item>
-            </v-list-item>
-        </v-list>
-      </v-card-text>
-    </v-card>
+    <v-treeview
+    :items="recipeInfo.items"
+    density="compact"
+    item-value="title"
+    activatable
+    open-on-click
+  >
+  </v-treeview>
   </v-container>
 </template>
