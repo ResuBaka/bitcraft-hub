9d03592fed718ca04fba48f267a5678f53c2c2b4
ResuBaka <resubaka@gmail.com>
2025-06-08 17:13:34 +0200

    fix(Recipe): asspect for cargo images and image path

--- frontend/app/pages/items/[type]/[id].vue
+++ frontend/app/pages/items/[type]/[id].vue
@@ -19,15 +19,14 @@ const { data: allRecipiesFetch, pending: allRecipiesPending } =
     return `${api.base}/recipes/get_all`;
   });
 
-
 const recipeInfo = computed(() => {
-  let hasValues =  allRecipiesFetch.value === undefined
+  let hasValues = allRecipiesFetch.value === undefined;
   let allRecipies = allRecipiesFetch.value?.recipes ?? {};
   let cargo_desc = allRecipiesFetch.value?.cargo_desc ?? {};
   let item_desc = allRecipiesFetch.value?.item_desc ?? {};
   let item_list_desc = allRecipiesFetch.value?.item_list_desc ?? {};
-  let id = route.params.id
-  let type =  route.params.type
+  let id = route.params.id;
+  let type = route.params.type;
   const consumed = {
     Item: {},
     Cargo: {},
@@ -36,26 +35,37 @@ const recipeInfo = computed(() => {
     Item: {},
     Cargo: {},
   };
-  function getCraftedItemStack(item_stack,recipie){
-      if (item_stack.item_type == "Item") {
-        if (crafted["Item"][item_stack.item_id] == undefined) {
-          crafted["Item"][item_stack.item_id] = [];
-        }
-        crafted["Item"][item_stack.item_id].push({id: recipie.id, quantity: item_stack.quantity});
-        if(item_desc[item_stack.item_id].item_list_id !== 0 && item_list_desc[item_desc[item_stack.item_id].item_list_id] !== undefined){
-        for(const possibility of item_list_desc[item_desc[item_stack.item_id].item_list_id]?.possibilities) {
-            for(const item of possibility.items){
-              getCraftedItemStack(item,recipie)
-            }
+  function getCraftedItemStack(item_stack, recipie) {
+    if (item_stack.item_type == "Item") {
+      if (crafted["Item"][item_stack.item_id] == undefined) {
+        crafted["Item"][item_stack.item_id] = [];
+      }
+      crafted["Item"][item_stack.item_id].push({
+        id: recipie.id,
+        quantity: item_stack.quantity,
+      });
+      if (
+        item_desc[item_stack.item_id].item_list_id !== 0 &&
+        item_list_desc[item_desc[item_stack.item_id].item_list_id] !== undefined
+      ) {
+        for (const possibility of item_list_desc[
+          item_desc[item_stack.item_id].item_list_id
+        ]?.possibilities) {
+          for (const item of possibility.items) {
+            getCraftedItemStack(item, recipie);
+          }
         }
       }
-      } else {
-        if (crafted["Cargo"][item_stack.item_id] == undefined) {
-          crafted["Cargo"][item_stack.item_id] = [];
-        }
-        crafted["Cargo"][item_stack.item_id].push({id: recipie.id, quantity: item_stack.quantity});
+    } else {
+      if (crafted["Cargo"][item_stack.item_id] == undefined) {
+        crafted["Cargo"][item_stack.item_id] = [];
       }
+      crafted["Cargo"][item_stack.item_id].push({
+        id: recipie.id,
+        quantity: item_stack.quantity,
+      });
     }
+  }
   for (const recipie of Object.values(allRecipies)) {
     for (const item_stack of Object.values(recipie.consumed_item_stacks)) {
       if (item_stack.item_type == "Item") {
@@ -71,11 +81,11 @@ const recipeInfo = computed(() => {
       }
     }
     for (const item_stack of Object.values(recipie.crafted_item_stacks)) {
-      getCraftedItemStack(item_stack, recipie)
+      getCraftedItemStack(item_stack, recipie);
     }
   }
 
-  if(hasValues){
+  if (hasValues) {
     return {
       items: [{}],
       consumed,
@@ -83,77 +93,93 @@ const recipeInfo = computed(() => {
     };
   }
 
-  let item
-  if(type == "Item"){
-      item = item_desc[id]
-  }else{
-    item = cargo_desc[id]
+  let item;
+  if (type == "Item") {
+    item = item_desc[id];
+  } else {
+    item = cargo_desc[id];
   }
 
   function getCraftedChildren() {
+    const children = [];
+    for (const recipe of crafted[type][id]) {
+      let itemChildren = [];
 
-    const children = []
-    for(const recipe of crafted[type][id]){
-      let itemChildren = []
-
-      for(const item of allRecipies[recipe.id]?.crafted_item_stacks){
+      for (const item of allRecipies[recipe.id]?.crafted_item_stacks) {
         itemChildren.push({
           id: item.item_id,
           type: item.item_type,
           quantity: 1,
-          children: getConsumedChildren(item.item_id,item.item_type,1,[]),
-        })
+          children: getConsumedChildren(item.item_id, item.item_type, 1, []),
+        });
       }
-       children.push({
-        children: itemChildren
-      })
-      
+      children.push({
+        children: itemChildren,
+      });
     }
-    return children
+    return children;
   }
 
-    function getQuantity(item_quantity: number, quantity: number, recipe_id_quantity:number) {
-      return Math.ceil((quantity * item_quantity) / recipe_id_quantity)
-    }
+  function getQuantity(
+    item_quantity: number,
+    quantity: number,
+    recipe_id_quantity: number,
+  ) {
+    return Math.ceil((quantity * item_quantity) / recipe_id_quantity);
+  }
 
-    function getConsumedChildren(id: any, type: "Item" | "Cargo", quantity: number,recipes: number[]) {
-    const children = []
-    if(crafted[type][id] == undefined){
-      return
+  function getConsumedChildren(
+    id: any,
+    type: "Item" | "Cargo",
+    quantity: number,
+    recipes: number[],
+  ) {
+    const children = [];
+    if (crafted[type][id] == undefined) {
+      return;
     }
-    for(const recipe of crafted[type][id]){
-      let itemChildren = []
-      const exists =
-        recipes.findIndex((value) => value == recipe.id) !== -1;
-      if(exists){
-        continue
+    for (const recipe of crafted[type][id]) {
+      let itemChildren = [];
+      const exists = recipes.findIndex((value) => value == recipe.id) !== -1;
+      if (exists) {
+        continue;
       }
       recipes.push(recipe.id);
-      for(const item of allRecipies[recipe.id].consumed_item_stacks){
-        let get_qauntity = getQuantity(item.quantity,quantity, recipe.quantity)
+      for (const item of allRecipies[recipe.id].consumed_item_stacks) {
+        let get_qauntity = getQuantity(
+          item.quantity,
+          quantity,
+          recipe.quantity,
+        );
         itemChildren.push({
           id: item.item_id,
           type: item.item_type,
           quantity: get_qauntity,
-          children: getConsumedChildren(item.item_id,item.item_type,get_qauntity, [...recipes]),
-        })
+          children: getConsumedChildren(
+            item.item_id,
+            item.item_type,
+            get_qauntity,
+            [...recipes],
+          ),
+        });
       }
       children.push({
         recipe: true,
-        children: itemChildren
-      })
-      
+        children: itemChildren,
+      });
     }
-    return children
+    return children;
   }
-  let children = getCraftedChildren()
-  let items = [{
+  let children = getCraftedChildren();
+  let items = [
+    {
       id: id,
-      type: type ,
+      type: type,
       quantity: 1,
       children: children,
-  }]
-    
+    },
+  ];
+
   return {
     items,
   };
