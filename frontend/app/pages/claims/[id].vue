5292267168b7d950cac460c6748763f7bdef7725
Boegie19 <34578426+Boegie19@users.noreply.github.com>
2025-06-16 17:45:15 +0200

    remove console.log

--- frontend/app/pages/claims/[id].vue
+++ frontend/app/pages/claims/[id].vue
@@ -63,7 +63,6 @@ const { data: InventoryChangelogFetch, refresh: InventoryChangelogRefresh } = us
   },{
     onRequest: ({ options }) => {
         options.query = options.query || {};
-        console.log(item_object.value)
         if (item_object.value !== undefined && item_object.value !== null) {
           options.query.item_id = item_object.value.id;
           options.query.item_type = item_object.value.type;
