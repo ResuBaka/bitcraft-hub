00f4254eded8344bce597ae286fd702895cdda6c
ResuBaka <resubaka@gmail.com>
2025-07-06 11:43:56 +0200

    refactor: move to chrono crate instead of time

--- rust/api-server/entity/src/shared/timestamp.rs
+++ rust/api-server/entity/src/shared/timestamp.rs
@@ -1,19 +1,18 @@
 use serde::{Deserialize, Serialize};
-use time::OffsetDateTime;
+use chrono::{DateTime, Utc};
 use ts_rs::TS;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, sea_orm::FromJsonQueryResult, TS)]
 pub struct Timestamp {
-    #[serde(with = "time::serde::timestamp::microseconds")]
     #[ts(as = "String")]
-    pub __timestamp_micros_since_unix_epoch__: OffsetDateTime,
+    pub __timestamp_micros_since_unix_epoch__: DateTime<Utc>,
 }
 
 impl From<spacetimedb_sdk::Timestamp> for Timestamp {
     fn from(value: spacetimedb_sdk::Timestamp) -> Self {
         Self {
-            __timestamp_micros_since_unix_epoch__: OffsetDateTime::from_unix_timestamp_nanos(
-                (value.to_micros_since_unix_epoch() * 1000) as i128,
+            __timestamp_micros_since_unix_epoch__: DateTime::from_timestamp_millis(
+                value.to_micros_since_unix_epoch(),
             )
             .expect("invalid Unix timestamp"),
         }
